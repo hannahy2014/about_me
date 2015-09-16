8d928377d8d41a10366e8e15372c7acade9dd59f
Hannah Mills <hannahymills@gmail.com>
2015-09-16 17:03:17 +0100

    content added

--- js/app.js
+++ js/app.js
@@ -1,45 +1,84 @@
-(function() {
-  var app = angular.module('aboutMeApp', []);
-  
-  app.controller("AboutMeController", function($scope) {
-      $scope.sections = sections;
-      $scope.selectedSection = sections[-1];
-      $scope.selectSection = function(section) {
-          $scope.selectedSection = section;
-      }
+var contactController = function ($scope)
+{
+  // Initialize the model variables
+  $scope.title = "Contact Details";
+  $scope.address = "Apartment 11, 3 Regent Terrace, Rita Road, London, SW8 1AW";
+  $scope.tel = "07818 064374";
+  $scope.email = "hannhymills@gmail.com"
+};
 
-  });
+var gaController = function ($scope)
+{
+  $scope.title = "Education";
+  $scope.dates = "Nov 2014 - Feb 2015";
+  $scope.school = "General Assembley";
+  $scope.qualification = "Web Development Immersive"
+};
 
-  sections = [
-  {
-    title: "Contact Details",
-    address: "Apartment 11, 3 Regent Terrace, Rita Road, London, SW8 1AW",
-    tel: "07818 064374"
-  },
-  {
-    title: "Education",
-    dates: "Nov 2014 - Feb 2015",
-    school: "General Assembly",
-    qualification: "Web Development Immersive",
+var universityController = function ($scope)
+{
+  $scope.dates = "Sept 2000 - May 2003";
+  $scope.school = "London South Bank University";
+  $scope.qualification = "2:1 BSc (Hons) Media and Society"
+};
 
-    dates2: "Sept 2000 - May 2003",
-    school2: "London South Bank University",
-    qualification2: "2:1 BSc (Hons) Media and Society",
+var careerController = function ($scope)
+{
+  $scope.title = "Career";
+  $scope.dates = "Mar 2015 - present";
+  $scope.position = "UX and Web Development Intern";
+  $scope.company = "Andiamo";
+  $scope.more = "Andiamo is creating a new user-centered children's orthotics service. The aim is to drastically reduce wait times and create beautiful high quality orthotics by using 3D scanning and printing technology. I joined Andiamo as a part-time junior web developer (unpaid internship) to get a feel of how software projects are run, from idea creation, to user research and development. I worked closely with the UX specialist working on prototyping a new, more user-focused, patient record system. We used paper prototyping, Balsamiq and Wordpress. I also had the opportunity to work with the lead developer who was working on a Node.js web application and API, running on the IBM Bluemix cloud platform. My role was to convert an existing Bootstrap and jQuery front-end to a Bootstrap and Angular.js front-end."
+};
 
-    dates3: "",
-    school3: "",
-    qualification3: ""
+var selfStudyController = function ($scope)
+{
+  $scope.dates = "Mar 2015 - present";
+  $scope.position = "Self-study";
+  $scope.more = "Following the advice from some developer friends, I focused my time after General Assembly on Javascript. I picked up Node.js as the server-side framework so I can build dynamic websites and devoted most of my time on Angular.js, HTML and CSS."
+};
 
-  },
-  {
-    title: "Career",
-    establishment: "Andiamo"
-  },
-  {
-    title: "Home",
-    something: "TEST"
-  }
-]
+var btgController = function ($scope)
+{
+  $scope.dates = "Jan 2013 - Aug 2014";
+  $scope.position = "Team Administrator & Project Coordinator";
+  $scope.company = "BTG International Ltd."
+  $scope.more = "BTG International Ltd. is a pharmaceuticals company based focusing on interventional and speciality medicines. I was hired to support the commercial team, but worked my way up to project coordinator for launch of a treatment for severe vascular conditions. A large part of my role was project planning and improving team collaboration. I created Gantt charts, status dashboards, streamlined processes and implemented a team task system. Another focus was to work with suppliers and manage the project stakeholders. I was the main point of contact for the external stakeholders and industry specialists."
+};
 
-})();
+var bbcController = function ($scope)
+{
+  $scope.dates = "Jun 2012 - Jan 2013";
+  $scope.position = "Project Management Office Support";
+  $scope.company = "BBC";
+  $scope.more = "The BBC has been reviewing the commercial relationships with their IT suppliers and put a team together to manage the change and transition to a mutli-sourcing strategy. I was responsible for implementing and maintaining the team collaboration and document exchange platform, preparing the communications and documents before and after workshops and overall project management office support (tracking actions and status, meeting invites, communications with organization-wide stakeholders, etc.)."
+};
+
+var angelsController = function ($scope)
+{
+  $scope.dates = "Apr 2012 - Jun 2012";
+  $scope.position = "Various temporary team support assignments";
+  $scope.company = "Office Angels";
+  $scope.more = "Temping whilst waiting for new BBC job opportunities to become available. I had a series of team support and admin roles with various digital agencies."
+};
+
+var bbcwwController = function ($scope)
+{
+  $scope.dates = "Sept 2011 - Mar 2012 (FTC)";
+  $scope.position = "Marketing Coordinator";
+  $scope.company = "BBC Worldwide/Immediate Media";
+  $scope.more = "I was responsible for supporting the delivery of consumer marketing and promotional strategies across the eleven titles within the pre-school portfolio. I launched a new preschool title myself, including event management, digital activities and consumer promotions. I presented competitor/sales analysis (partly based on Google Analytics data) and provided recommendations for future strategy. I liaised with the central buying team, editorial and stakeholders to deliver all promotional materials, magazine covers and gift designs to strict deadlines."
+};
+
+var caseController = function ($scope)
+{
+  $scope.dates = "Jun 2003 - Aug 2011";
+  $scope.position = "Media Project Coordinator & Customer Support Officer";
+  $scope.company = "Case Centre";
+  $scope.more = "A demanding and diverse role in which I had two distinct areas of responsibility; foremost to manage the production of corporate marketing materials; secondly to provide excellent customer service support to a global and demanding customer base. I was writing briefs, filming, editing (Final Cut Pro), uploading to a YouTube Channel plus managing all logistical issues. I updated Case Centre website content as required and produced a database of footage. Last but not least, I managed media project budget, monitored and reported on ROI and trained new team members."
+};
+
+
+
+
 
---
+++ js/sections.js
@@ -0,0 +1,65 @@
+  sections = [
+  {  
+    title: "Contact Details",
+
+    address: "Apartment 11, 3 Regent Terrace, Rita Road, London, SW8 1AW",
+    tel: "07818 064374"
+  },
+  {
+    title: "Education",
+
+    dates: "Nov 2014 - Feb 2015",
+    school: "General Assembly",
+    qualification: "Web Development Immersive",
+
+    dates2: "Sept 2000 - May 2003",
+    school2: "London South Bank University",
+    qualification2: "2:1 BSc (Hons) Media and Society"
+  },
+  {
+    title: "Career",
+
+    dates3: "Mar 2015 - present",
+    position: "UX and Web Development Intern",
+    company: "Andiamo",
+
+    dates4: "Mar 2015 - present",
+    position2: "Self-study",
+    
+    
+  }
+  ];
+
+  // careers = [
+  // {
+  //   title: "Career",
+
+  //   dates: "" 
+
+  // }
+  // ];
+  
+  //each time a new "section" is added, a new highlighted box appears on the list of sections.
+ 
+
+
+
+  // educations = [
+  // {
+  //   title: "Education",
+  //   dates: "Nov 2014 - Feb 2015",
+  //   school: "General Assembly",
+  //   qualification: "Web Development Immersive"
+  // },
+  // {
+  //   dates: "Sept 2000 - May 2003",
+  //   school: "London South Bank University",
+  //   qualification: "2:1 BSc (Hons) Media and Society",
+  // }]; 
+
+
+
+  
+
+
+  
